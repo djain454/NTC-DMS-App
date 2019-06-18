0afbd7899927e28c26eb60fdaa83cbe8431ff50f
djain454 <37479836+djain454@users.noreply.github.com>
2019-06-18 13:18:27 +0530

    Code Optimize

--- app/src/main/java/com/deepak/ntcdms/MainActivity.java
+++ app/src/main/java/com/deepak/ntcdms/MainActivity.java
@@ -27,13 +27,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         firebaseAuth = FirebaseAuth.getInstance();
         usernameET = (EditText) findViewById(R.id.editText1);
         passwordET = (EditText) findViewById(R.id.editText2);
-        //passwordET.setTransformationMethod(new AsteriskPasswordTransformationMethod());
-
+        
     }
 
     public void SignIn(View view) {
